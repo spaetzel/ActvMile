7c3bfe093b2f8d9ba08b14cd686db5a7536341aa
Will Spaetzel <will@spaetzel.com>
2011-12-27 20:20:20 -0500

    start building workout object

--- actvmile.js
+++ actvmile.js
@@ -9,34 +9,49 @@ https://motoactv.com/data/workoutDetail.json?workoutActivityId=23t3eoKlRcm6WbobH
 		*/
 		
 		
+
+
+function formatTime(timestamp){
+	// create a new javascript Date object based on the timestamp
+	
+	var date = new Date(timestamp);
+
+	
+	var formattedTime =  date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + "T" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "Z";
+	
+	return formattedTime;
+}		
+		
 function getShareUrl(id, callback) {
 
-			var postData = {
-				workoutActivityId: id
-			};
-			
-			$.ajax({
-				url: "/sharing/shareWorkout.json",
-				type: 'POST',
-				data: postData,
-				dataType: 'json',
-				success: function(json) {
-					$("#socialWaitDialog").dialog("close");
+	var postData = {
+		workoutActivityId: id
+	};
+	
+	$.ajax({
+		url: "/sharing/shareWorkout.json",
+		type: 'POST',
+		data: postData,
+		dataType: 'json',
+		success: function(json) {
+			$("#socialWaitDialog").dialog("close");
 
-					if (json && ((typeof json.code == "undefined") || (json.code > -1))) {
-						var urlToShare = json.short_url;
-					
-						
-						callback(urlToShare);
-					}
-					else {
-						alert("Error getting share url");
-					}
-				}
-			});
-		};
-		
-		
+			if (json && ((typeof json.code == "undefined") || (json.code > -1))) {
+				var urlToShare = json.short_url;
+			
+				
+				callback(urlToShare);
+				
+				
+			}
+			else {
+				alert("Error getting share url");
+			}
+		}
+	});
+};
+
+
 
 var workoutId = "23t3eoKlRcm6WbobHNJmRQ==";
 var detailsUrl = "https://motoactv.com/data/workoutDetail.json?workoutActivityId=";
@@ -51,19 +66,46 @@ var detailsUrl = "https://motoactv.com/data/workoutDetail.json?workoutActivityId
 		data: { username: encodeURI('motorola@redune.com') },
 		success: function(data){
 
-			var name = data.journaldata.journalname;
-			var notes = data.journaldata.journalnotes;
-			
-			var distance = data.summary.DISTANCE;
-			
 			var startTime = data.summary.STARTTIME;
 			var endTime = data.summary.ENDTIME;
+
+			var entry = {
+				lat: data.route[0].LATITUDE,
+				lon: data.route[0].LONGITUDE,
+				workout: {
+					activity_type: "running",
+					completed_at: formatTime( endTime ),
+					distance: {
+						value: endTime, 
+						units: "kilometers"
+					},
+					duration: ( endTime - startTime ) / 1000,
+					calories: data.summary.CALORIEBURN,
+					title: data.journaldata.journalname
+				}
+			};
+			
+			alert(entry.workout.title);
+				/*
+
+
+workout[title], string (optional)
+optional title for a workout
+workout[route_id], integer (optional)
+the id of the route associated with this workout (see route docs)
+*/
+
+			var name = ;
+			var notes = data.journaldata.journalnotes;
+			
+			
+			
+			//var endTime = data.summary.ENDTIME;
 			
 			var elapsedTime = endTime - startTime;
 
 			getShareUrl(workoutId, function(url){
 				alert(url);
-				window.open(url);
 			});
 			
 		},
